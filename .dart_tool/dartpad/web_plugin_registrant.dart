66d26bd9cf41457643afc766a1d71c85a18ec4f8
Anas Elbattra <75434006+AnasElbattra@users.noreply.github.com>
2024-10-22 06:47:16 +0300

    fix crowdin

--- .dart_tool/dartpad/web_plugin_registrant.dart
+++ .dart_tool/dartpad/web_plugin_registrant.dart
@@ -6,7 +6,7 @@
 // @dart = 2.13
 // ignore_for_file: type=lint
 
-import 'package:app_links_web/app_links_web.dart';
+import 'package:app_links/src/app_links_web.dart';
 import 'package:connectivity_plus/src/connectivity_plus_web.dart';
 import 'package:shared_preferences_web/shared_preferences_web.dart';
 import 'package:url_launcher_web/url_launcher_web.dart';
